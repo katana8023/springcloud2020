f92dceff620f9c6d4c5764e849d46645eec527f8
katana8023 <katana8023@163.com>
2020-04-29 14:23:43 +0800

    cloud demo

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/gx/springcloud/PaymentMain8001.java
@@ -0,0 +1,14 @@
+package com.gx.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GuoX
+ * @email katana8023@163.com
+ * @date 2020/4/29 14:21
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+
+}
